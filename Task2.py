0be3a1e6989d5fe74b847efab297da9ef25fe799
Ishu Kapoor <51852318+IshuKapoor@users.noreply.github.com>
2025-06-20 22:43:54 +0530

    Task2.py

---
+++ Task2.py
@@ -0,0 +1,6 @@
+first_name = input("enter your first name:")
+second name = input("enter your second name:")
+
+full_name=first_name + " " + last_name
+
+print(f"Hello,{full_name}! Welcome to the Python World!")
